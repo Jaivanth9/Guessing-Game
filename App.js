26810bc5fcbec0681ef7a79206016b88e131b395
Jaivanth Koppula <jaivanthkoppula999@gmail.com>
2025-07-01 10:38:40 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [userNumber, setUserNumber] = useState(null);
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (rounds) => {
+    setGuessRounds(rounds);
+  };
+
+  const restartGameHandler = () => {
+    setUserNumber(null);
+    setGuessRounds(0);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds === 0) {
+    content = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={restartGameHandler}
+      />
+    );
+  }
+
+  return <SafeAreaView style={styles.screen}>{content}</SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
